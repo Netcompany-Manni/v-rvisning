56ac6ed6cfe00a2dea59e64269bedb053d155aaf
Manveer Singh Pandher <manvep@netcompany.com>
2018-11-27 14:40:16 +0100

    la til et utropstegn

--- src/WeatherComponents/WeatherList.js
+++ src/WeatherComponents/WeatherList.js
@@ -6,7 +6,7 @@ const weatherList = (props) => {
     return (
         <div id="weather_list">
             {props.results.map((singleWeatherDetail, index)=>{
-                if(index === 0){singleWeatherDetail.applicable_date = "Today"};
+                if(index === 0){singleWeatherDetail.applicable_date = "Today!"};
                 if(index === 1){singleWeatherDetail.applicable_date = "Tomorrow"};
                 return <div>
                     <SingleWeatherComp data={singleWeatherDetail}/>
